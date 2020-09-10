607ff8128bb8ffa2475780f75d952ce4a6019b58
Debojeet Das <debojeetofficial@gmail.com>
2020-09-10 16:39:00 +0000

    Spliting of the operands and the operator done

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,4 +1,27 @@
-def arithmetic_arranger(problems):
+def arithmetic_arranger(problems, sum = False):
+#This checks the number of input problems
+  if len(problems) > 5 :
+    print('Error: Too many problems.')
+    exit()
 
+#Initialization of lists
+  foper = list()
+  soper = list()
+  opera = list()
 
-    return arranged_problems
+#Spliting the operands in the problems  
+  for problem in problems:
+    problem.lstrip()
+    problem.rstrip()
+    sproblem = problem.split(" ")
+    if len(sproblem[0]) > 4 or len(sproblem[2]) > 4:
+      print('Error: Numbers cannot be more than four digits.')
+      exit()
+    if not (sproblem[1] == '+' or sproblem[1] == '-'):
+      print('Error: Operator must be \'+\' or \'-\'.')
+      exit() 
+    foper.append(sproblem[0])
+    opera.append(sproblem[1])
+    soper.append(sproblem[2])
+
+  return arranged_problems
